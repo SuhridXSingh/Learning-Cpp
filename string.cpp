effe49cefe766274d8e55dc6409dbb10c16a2764
Suhrid Singh <suhrids88@gmail.com>
2025-12-16 21:00:03 +0530

    A program to take string inputs from the user and print the strings.

---
+++ string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    char n[100], m[100];
+    
+    cin >> n >> m;
+    cout<< n<< endl << m ;
+    
+    return 0;
+}
